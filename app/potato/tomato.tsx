4ebf4dfb9c51915eb7b56bbf2bce18d8a9be5f5e
Jay Son <mooul0529@naver.com>
2025-02-21 00:15:34 +0900

    #1.3 Layout Component

--- app/potato/tomato.tsx
+++ app/potato/tomato.tsx
@@ -1,3 +1,10 @@
 export default function AboutUs() {
   return <div>About Us</div>;
 }
+
+export const links = () => [{ rel: "stylesheet", href: "potato.com" }];
+
+export const meta = () => [
+  { title: "Tomato" },
+  { name: "description", content: "Tomato" },
+];
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"), route("/about", "potato/tomato.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("/about", "potato/tomato.tsx"),
+] satisfies RouteConfig;
