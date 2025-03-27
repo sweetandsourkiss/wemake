11b1cc782e749551df64aca124a25477266e7253
Jay Son <mooul0529@naver.com>
2025-03-27 17:53:38 +0900

    #1.4 Tailwind

--- app/potato/tomato.tsx
+++
@@ -1,9 +0,0 @@
-export default function AboutUs() {
-  return <div>AboutUs</div>;
-}
-
-export const links = () => [{ rel: "stylesheet", href: "potato.com" }];
-
-export const meta = () => {
-  return [{ title: "Tomato" }, { name: "description", content: "Tomato" }];
-};
