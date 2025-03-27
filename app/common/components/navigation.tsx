14a86e44bcf478cbd601977f98f6d8b3536746d3
Jay Son <mooul0529@naver.com>
2025-03-27 20:54:32 +0900

    #3.1 Navigation part One

---
+++ app/common/components/navigation.tsx
@@ -0,0 +1,151 @@
+import { Link } from "react-router";
+import { Separator } from "./ui/separator";
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuTrigger,
+  NavigationMenuLink,
+  NavigationMenuContent,
+} from "./ui/navigation-menu";
+
+const menus = [
+  {
+    name: "Products",
+    to: "/products",
+    items: [
+      {
+        name: "Leaderboards",
+        description: "See the top performers in our community",
+        to: "/products/leaderboards",
+      },
+      {
+        name: "Categories",
+        description: "See the top categories in our community",
+        to: "/products/categories",
+      },
+      {
+        name: "Search",
+        description: "Search for a product",
+        to: "/products/search",
+      },
+      {
+        name: "Submit a Product",
+        description: "Submit a product to our community",
+        to: "/products/submit",
+      },
+      {
+        name: "Promote a Product",
+        description: "Promote a product to our community",
+        to: "/products/promote",
+      },
+    ],
+  },
+  {
+    name: "Jobs",
+    to: "/jobs",
+    items: [
+      {
+        name: "Remote Jobs",
+        description: "See the remote jobs in our community",
+        to: "/jobs?location=remote",
+      },
+      {
+        name: "Full-Time Jobs",
+        description: "See the full-time jobs in our community",
+        to: "/jobs?type=full-time",
+      },
+      {
+        name: "Freelance Jobs",
+        description: "See the freelance jobs in our community",
+        to: "/jobs?type=freelance",
+      },
+      {
+        name: "Internship",
+        description: "See the internship jobs in our community",
+        to: "/jobs?type=internship",
+      },
+      {
+        name: "Submit a Job",
+        description: "Submit a job to our community",
+        to: "/jobs/submit",
+      },
+    ],
+  },
+  {
+    name: "Community",
+    to: "/community",
+    items: [
+      {
+        name: "All Posts",
+        description: "See all posts in our community",
+        to: "/community",
+      },
+      {
+        name: "Top Posts",
+        description: "See the top posts in our community",
+        to: "/community?sort=top",
+      },
+      {
+        name: "New Posts",
+        description: "See the new posts in our community",
+        to: "/community?sort=new",
+      },
+      {
+        name: "Create a Post",
+        description: "Create a post in our community",
+        to: "/community/create",
+      },
+    ],
+  },
+  {
+    name: "IdeasGPT",
+    to: "/ideas",
+  },
+  {
+    name: "Teams",
+    to: "/teams",
+    items: [
+      {
+        name: "All Teams",
+        description: "See all teams in our community",
+        to: "/teams",
+      },
+      {
+        name: "Create a Team",
+        description: "Create a team in our community",
+        to: "/teams/create",
+      },
+    ],
+  },
+];
+export default function Navigation() {
+  return (
+    <nav className="flex px-20 h-16 items-center justify-between backdrop-blur fixed top-0 left-0 right-0 z-50 bg-background/50">
+      <div className="flex items-center">
+        <Link to="/" className="font-bold tracking-tighter text-lg">
+          WeMake
+        </Link>
+      </div>
+      <Separator orientation="vertical" className="h-6 mx-4" />
+      <NavigationMenu>
+        <NavigationMenuList>
+          {menus.map((menu) => (
+            <NavigationMenuItem key={menu.name}>
+              <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                {menu.items?.map((item) => (
+                  <NavigationMenuItem key={item.name}>
+                    <NavigationMenuLink asChild>
+                      <Link to={item.to}>{item.name}</Link>
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+                ))}
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </nav>
+  );
+}
