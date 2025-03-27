3a4613d0d3070a75ae33a6d64631cf8dd707cb1b
Jay Son <mooul0529@naver.com>
2025-03-27 20:21:19 +0900

    #2.5 Composer

---
+++ app/common/pages/home-page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main className="container mx-auto px-4 py-8">
+      <p className="text-lg text-gray-600">
+        This is the home page of your application.
+      </p>
+    </main>
+  );
+}
