7b6c829459c72798f915849c090411cf6c74f083
Jay Son <mooul0529@naver.com>
2025-03-29 22:01:48 +0900

    #3.6 Post Card

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,5 +1,7 @@
-import type { MetaFunction } from "react-router";
+import { Link, type MetaFunction } from "react-router";
 import { ProductCard } from "../../features/products/components/product-card";
+import { PostCard } from "../../features/community/components/post-card";
+import { Button } from "../components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,7 +12,7 @@ export const meta: MetaFunction = () => {
 
 export default function HomePage() {
   return (
-    <div className="px-20">
+    <div className="px-20 space-y-40">
       <div className="grid grid-cols-3 gap-4">
         <div>
           <h2 className="text-5xl font-bold leading-tight tracking-tight">
@@ -19,6 +21,9 @@ export default function HomePage() {
           <p className="text-xl font-light text-foreground">
             The best products made by our community today.
           </p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/products/leaderboards">Explore all products &rarr;</Link>
+          </Button>
         </div>
         {Array.from({ length: 10 }).map((_, index) => (
           <ProductCard
@@ -29,6 +34,29 @@ export default function HomePage() {
             commentsCount={12}
             viewsCount={12}
             upvotesCount={120}
+          />
+        ))}
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Latest Discussions
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            The latest products from our community.
+          </p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/community">Explore all discussions &rarr;</Link>
+          </Button>
+        </div>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <PostCard
+            id={`postId-${index}`}
+            title="What is the best way to learn React?"
+            author="Nico"
+            authorAvatarUrl="https://github.com/apple.png"
+            category="productivity"
+            postedAt="12 hours ago"
           />
         ))}
       </div>
---
+++ app/features/community/components/post-card.tsx
@@ -0,0 +1,58 @@
+import { Link } from "react-router";
+import { Button } from "../../../common/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "../../../common/components/ui/card";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "../../../common/components/ui/avatar";
+
+interface PostCardProps {
+  id: string;
+  title: string;
+  author: string;
+  authorAvatarUrl: string;
+  category: string;
+  postedAt: string;
+}
+
+export function PostCard({
+  id,
+  title,
+  author,
+  authorAvatarUrl,
+  category,
+  postedAt,
+}: PostCardProps) {
+  return (
+    <Link to={`/community/${id}`}>
+      <Card className="bg-transparent hover:bg-card/50 transition-colors">
+        <CardHeader className="flex flex-row items-center gap-2">
+          <Avatar className="size-14">
+            <AvatarFallback>{author}</AvatarFallback>
+            <AvatarImage src={authorAvatarUrl} />
+          </Avatar>
+          <div className="space-y-2">
+            <CardTitle>{title}</CardTitle>
+            <div className="flex gap-2 text-sm leading-tight text-muted-foreground">
+              <span>{author} on</span>
+              <span>{category}</span>
+              <span>·</span>
+              <span>{postedAt}</span>
+            </div>
+          </div>
+        </CardHeader>
+        <CardFooter className="flex justify-end">
+          <Button variant="link" asChild>
+            <Link to={`/community/${id}`}>Reply &rarr;</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
+}
